db22da61f649bf040ff57b7c28be6ba5f0ae4a27
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-04-29 17:52:01 +0200

    Initial docker-compose rework

--- _compose_prod.sh
+++ _compose_prod.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-docker-compose -f docker-compose.prod.yml build --parallel
+
+echo "Please input a NODE_AUTH_TOKEN for access to the Github Package Registry:"
+
+read NODE_AUTH_TOKEN
+
+docker-compose -f docker-compose.prod.yml build --parallel --build-arg NODE_AUTH_TOKEN=$NODE_AUTH_TOKEN
 docker-compose -f docker-compose.prod.yml up
