1189f7df819bc190ff096bebb6deaf6ff65c0c16
Roland Schläfli <rolandschlaefli@gmail.com>
2020-05-19 16:13:25 +0200

    Rework the docker-compose and run scripts (#67)

--- _run.sh
+++ _run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 NODE_AUTH_TOKEN=""
+OMDB_API_KEY=""
 ENVIRONMENT=""
 SERVICES="ionic_app nest_api poster_service metadata_service metadata_db"
 
@@ -10,29 +11,42 @@ for var in "$@"; do
     ENVIRONMENT=".prod"
     ;;
   "--init")
-    OMDB_API_KEY=""
     SERVICES=""
 
     echo "Please input a NODE_AUTH_TOKEN for access to the Github Package Registry:"
     read NODE_AUTH_TOKEN
-    echo $NODE_AUTH_TOKEN > node-auth.secret
+    echo $NODE_AUTH_TOKEN > node_auth.secret
 
     echo "Please input an OMDB API key for the poster-service"
     read OMDB_API_KEY
-    export OMDB_API_KEY
-    envsubst < docker-compose$ENVIRONMENT.yml > docker-compose$ENVIRONMENT.yml.secret
+    echo $OMDB_API_KEY > omdb_api.secret
     ;;
   esac
   shift
 done
 
-# if no auth token has been initialized yet, try to read it from secret
-if [ "$NODE_AUTH_TOKEN" = "" ]; then
-    NODE_AUTH_TOKEN=`cat node-auth.secret`
+if test -f "node_auth.secret"; then
+    NODE_AUTH_TOKEN=`cat node_auth.secret`
+    export NODE_AUTH_TOKEN
+fi
+
+if test -f "omdb_api.secret"; then
+    OMDB_API_KEY=`cat omdb_api.secret`
+    export OMDB_API_KEY
+fi
+
+if test -z "$NODE_AUTH_TOKEN"; then
+    echo "Please ensure that a valid NODE_AUTH_TOKEN is provided (run ./_run --init)"
+    exit
+fi
+
+if test -z "$OMDB_API_KEY"; then
+    echo "Please ensure that a valid OMDB_API_KEY is provided (run ./_run --init)"
+    exit
 fi
 
 # run a docker-compose build to ensure images are up-to-date
-docker-compose -f docker-compose$ENVIRONMENT.yml.secret build --parallel --build-arg NODE_AUTH_TOKEN=$NODE_AUTH_TOKEN
+docker-compose -f docker-compose$ENVIRONMENT.yml build --parallel
 
 # start the services as specified
-docker-compose -f docker-compose$ENVIRONMENT.yml.secret up $SERVICES
+docker-compose -f docker-compose$ENVIRONMENT.yml up $SERVICES
